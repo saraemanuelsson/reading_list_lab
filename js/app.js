76b49751dc380508905ca80f6b7b508288d20705
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-18 15:42:11 +0100

    Add delete all button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
+
+  const deleteAllButton = document.createElement("button");
+  deleteAllButton.textContent = "Delete All";
+  form.appendChild(deleteAllButton);
+  deleteAllButton.addEventListener('click', handleDeleteAll);
+
 })
 
 const handleFormSubmit = function(event) {
@@ -15,3 +21,8 @@ const handleFormSubmit = function(event) {
 const resetForm = function() {
   document.querySelector('#new-item-form').reset();
 }
+
+const handleDeleteAll = function(event) {
+  document.querySelectorAll("li").forEach(book => book.remove());
+};
+
